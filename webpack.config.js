dc36b6fa469208f5d7dbeb5fb50151842c6064e5
stdyrm <17044124+stdyrm@users.noreply.github.com>
2020-07-13 18:51:26 -1000

    fixed webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,10 @@
 const path = require("path");
-const isDevelopment = process.env.NODE_ENV === "development";
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: "./src/index.js",
-	devtool: "inline-source-map",
-	devServer: {
-		contentBase: "./dist"
-	},
 	output: {
-		path: `${__dirname}/dist`,
-		publicPath: "/",
+		path: path.join(__dirname, "/dist"),
 		filename: "bundle.js"
 	},
 	module: {
@@ -32,13 +26,13 @@ module.exports = {
 				test: /\.(woff|woff2|eot|ttf|otf)$/,
 				use: [
 					"file-loader"
-					]
+				]
 			}
 		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.resolve("./dist/index.html")
+			template: "./src/index.html"
 		})
 	]
 };
